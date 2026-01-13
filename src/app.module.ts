4ffaa6f16ac380c90a0e7a53f3b22f5513433604
ak <5795195+ak1987@users.noreply.github.com>
2026-01-13 20:42:12 +0500

    added dependencies / empty modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { VisitorsModule } from './visitors/visitors.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, VisitorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
