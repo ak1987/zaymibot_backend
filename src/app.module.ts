e0a3ea18344f1996585ecfa7286cdf3d8037c6c5
ak <5795195+ak1987@users.noreply.github.com>
2026-01-13 21:13:58 +0500

    added empty telegram module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { VisitorsModule } from './visitors/visitors.module';
+import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-  imports: [UsersModule, VisitorsModule],
+  imports: [UsersModule, VisitorsModule, TelegramModule],
   controllers: [AppController],
   providers: [AppService],
 })
